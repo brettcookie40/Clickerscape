2385b14670e2e306e4a2f77a546eae8622ee48c1
Brett Husar <bhusar@ceelectronics.com>
2022-01-17 16:35:34 -0500

    added the double up button. Revised how buttons were being drawn as well as how button clicks are detected

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import pygame
 import time
 
 #Initialize pygame
-
 pygame.init()
 
 #defining variables
@@ -36,8 +35,6 @@ def autoclick():
 def double_boost(autog):
     autog = autog * 2;
 
-
-
 def DrawText(text, Textcolor, Rectcolor, x, y, fsize):
     font = pygame.font.Font('freesansbold.ttf', fsize)
     text = font.render(text, True, Textcolor, Rectcolor)
@@ -57,9 +54,17 @@ def main_loop():
     global color2
     global color3   
     global coins
+    buttons = {
+        'auto_clicker_button' : pygame.Rect(50, 400, 200, 300),
+        'clicker_button' : pygame.Rect(350, 250, 100, 100),
+        'upgrade_clicker_button' : pygame.Rect(600, 317, 200, 300),
+        'double_up_button' : pygame.Rect(278, 571, 148, 20)
+        }
+
     mong = 1
     cost = 50
     cost2 = 50
+    cost3 = 500
     game_running = True
 
     while game_running:
@@ -71,25 +76,63 @@ def main_loop():
             
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mopos = pygame.mouse.get_pos()
-                if mopos >= (350, 0):
-                    if mopos <= (450, 0):
-                        coins += mong
+                print(mopos)
 
-                if mopos <= (800, 0):
-                    if mopos >= (600, 0):
-                        if coins >= cost:
-                            coins = coins - cost
-                            cost = cost * 1.5
-                            mong = mong * 1.1
-                            cost = round(cost, 0)
+                if(event.button == 1): #Left mouse button
+                    for button in buttons:
+                        # print(buttons)
+                        # print(button)
+                        if buttons[button].collidepoint(event.pos):
+                            if(button == 'clicker_button'):
+                                coins += mong
+                                print(button)
+                            elif(button == 'upgrade_clicker_button'):
+                                if coins >= cost:
+                                    coins = coins - cost
+                                    cost = cost * 1.5
+                                    mong = mong * 1.1
+                                    cost = round(cost, 0)
+                                print(button)
+                            elif(button == 'auto_clicker_button'):
+                                if coins >= cost2:
+                                    coins = coins - cost2
+                                    cost2 = cost2 * 1.5
+                                    autog = autog + 0.5
+                                    cost2 = round(cost2, 0)
+                                print(button)
+                            elif(button == 'double_up_button'):
+                                if(coins >= cost3):
+                                    coins = coins - cost3
+                                    cost3 = cost3 * 5
+                                    autog = autog * 2
+                                    cost3 = round(cost3, 0)
+                                print(button)
+                            else:
+                                print("no button clicked")
 
-                if mopos >= (50, 0):
-                    if mopos <= (245, 0):
-                        if coins >= cost2:
-                            coins = coins - cost2
-                            cost2 = cost2 * 1.5
-                            autog = autog + 0.5
-                            cost2 = round(cost2, 0)
+                # if mopos >= (350, 0):
+                #     if mopos <= (450, 0):
+                #         coins += mong
+
+                # if mopos <= (800, 0):
+                #     if mopos >= (600, 0):
+                #         if coins >= cost:
+                #             coins = coins - cost
+                #             cost = cost * 1.5
+                #             mong = mong * 1.1
+                #             cost = round(cost, 0)
+
+                # if mopos >= (50, 0):
+                #     if mopos <= (245, 0):
+                #         if coins >= cost2:
+                #             coins = coins - cost2
+                #             cost2 = cost2 * 1.5
+                #             autog = autog + 0.5
+                #             cost2 = round(cost2, 0)
+
+                #     if mopos >= (278, 542) and mopos <= (424, 561):
+                #             print("double up button! A")                        
+
 
                 if coins == 2147483647:
                     print("You Beat the game!")
@@ -97,14 +140,21 @@ def main_loop():
 
         #Draw stuff
         gameDisplay.fill(grey)
+
+        for button in buttons:
+            if(button == 'clicker_button'):
+                print("pain") 
+                pygame.draw.rect(gameDisplay,black, buttons[button])
+            else:
+                pygame.draw.rect(gameDisplay,light_grey, buttons[button])
+
         DrawText("Clicker Scape", black, white, 400, 100, 50)
         DrawText("You have " + str(f'{coins:.2f}') + " coins", black, white, 100, 50, 20)
         DrawText("Upgrade clicker " + str(cost), black, white, 700, 300, 20)
-        DrawText("Buy auto miner " + str(cost2), black, white, 150, 370, 20)
+        DrawText("Buy auto clicker " + str(cost2), black, white, 150, 370, 20)
+        DrawText("Double Up! " + str(cost3), black, white, 350, 550, 20)
         DrawText("Version: " + ver, black, white, 650, 50, 20)
-        rectangle(gameDisplay, light_grey, 50, 400, 200, 300)
-        rectangle(gameDisplay, black, 350, 250, 100, 100)
-        rectangle(gameDisplay, light_grey, 600, 317, 200, 300)
+
         pygame.display.update()
         clock.tick(60)                        
 

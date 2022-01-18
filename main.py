1cfc65592c02ce50ab71d510e32e807434f3bae9
Brett Husar <bhusar@ceelectronics.com>
2022-01-18 11:57:28 -0500

    Fix some spacing. Add Golden nuggets. Change balancing on auto click and manual click. Added double up button

--- main.py
+++ main.py
@@ -5,37 +5,11 @@ from ssl import Options
 from threading import Timer
 import pygame, time, random
 
-class RepeatedTimer(object):
-    def __init__(self, interval, function, *args, **kwargs):
-        self._timer     = None
-        self.interval   = interval
-        self.function   = function
-        self.args       = args
-        self.kwargs     = kwargs
-        self.is_running = False
-        self.start()
-
-    def _run(self):
-        self.is_running = False
-        self.start()
-        self.function(*self.args, **self.kwargs)
-
-    def start(self):
-        if not self.is_running:
-            self._timer = Timer(self.interval, self._run)
-            self._timer.start()
-            self.is_running = True
-
-    def stop(self):
-        self._timer.cancel()
-        self.is_running = False
-
 #Initialize pygame
 pygame.init()
 
 #defining variables
 clock = pygame.time.Clock()
-# s = sched.scheduler(time.time, time.sleep)
 ver = "0.0.0"
 autog = 0
 coins = 0
@@ -46,6 +20,7 @@ black = (0, 0, 0)
 grey = (128, 128, 128)
 light_grey = (224, 224, 224)
 yellow = ((255,255,0))
+moon_glow = ((235,245,255))
 
 # creating display and caption
 gameDisplay = pygame.display.set_mode((display_width, display_height));
@@ -63,13 +38,11 @@ def double_boost(autog):
 
 def generate_nugget(buttons, gameDisplay):
     start = 1
-    end = 5000
+    end = 9696
     x = random.randint(start,end)
-    print(buttons['golden_nugget'])
-    if x > 4000:
-        print("x greater than 4k")
-        pygame.draw.rect(gameDisplay, yellow, buttons['golden_nugget'])
-        pygame.display.update()
+    # if x > 8000:
+    if x >= 696 and x <= 721:
+        print("Ooooh! A Golden Nugget!")
         return True
     return False
         
@@ -94,67 +67,89 @@ def main_loop():
     global color3   
     global coins
     gameDisplay.fill(grey)
+    dt = clock.get_time() #keeping track of time between previous tick check
     buttons = {
         'auto_clicker_button' : pygame.Rect(50, 400, 200, 300),
         'clicker_button' : pygame.Rect(350, 250, 100, 100),
-        'upgrade_clicker_button' : pygame.Rect(600, 317, 200, 300),
-        'double_up_button' : pygame.Rect(278, 571, 148, 20),
-        'golden_nugget' : pygame.Rect(50, 400, 50, 50)
+        'upgrade_clicker_button' : pygame.Rect(550, 317, 200, 300),
+        'double_up_button' : pygame.Rect(327, 571, 148, 20),
+        'golden_nugget' : pygame.Rect(130, 200, 50, 50)
         }
-
+    
+    is_showing_nugget = False
     mong = 1
-    cost = 50
-    cost2 = 50
+    cost = 10
+    cost2 = 25
     cost3 = 500
+
     game_running = True
-    rt = RepeatedTimer(1, generate_nugget, buttons, gameDisplay)
+    # rt = RepeatedTimer(1, generate_nugget, buttons, gameDisplay)
 
     while game_running:
+        #golden nuggets
+        if(is_showing_nugget == True):
+            dt = dt + clock.get_time()
+            if(dt > 10000): #Remove nugget after 10 seconds
+                print("You were too slow!")
+                is_showing_nugget = False
+                dt = 0
+        else:
+            dt = dt + clock.get_time()
+            if(dt > 1000):
+                is_showing_nugget = generate_nugget(buttons, gameDisplay)
+                dt = 0
+
         if game_running:
             autoclick()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                rt.stop()
                 game_running = False
             
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mopos = pygame.mouse.get_pos()
-                print(mopos)
+                # print(mopos)
 
                 if(event.button == 1): #Left mouse button
                     for button in buttons:
-                        # print(buttons)
-                        # print(button)
                         if buttons[button].collidepoint(event.pos):
                             if(button == 'clicker_button'):
                                 coins += mong
-                                print(button)
+                                # print(button)
                             elif(button == 'upgrade_clicker_button'):
                                 if coins >= cost:
                                     coins = coins - cost
-                                    cost = cost * 1.5
-                                    mong = mong * 1.1
+                                    cost = cost * 1.75
+                                    mong = mong * 1.5
                                     cost = round(cost, 0)
-                                print(button)
+                                # print(button)
                             elif(button == 'auto_clicker_button'):
                                 if coins >= cost2:
                                     coins = coins - cost2
                                     cost2 = cost2 * 1.5
                                     autog = autog + 0.5
                                     cost2 = round(cost2, 0)
-                                print(button)
+                                # print(button)
                             elif(button == 'double_up_button'):
                                 if(coins >= cost3):
-                                    coins = coins - cost3
-                                    cost3 = cost3 * 5
-                                    autog = autog * 2
-                                    cost3 = round(cost3, 0)
-                                print(button)
-                            elif(button == 'golden_nugget'):
-                                coins += 50000
-                                print("HYPE")
+                                    if(autog == 0): #If user hasn't purchased auto clicker, activate their autoclicker at 1*2 speed
+                                        coins = coins - cost3
+                                        cost3 = cost3 * 5
+                                        autog = (autog + 1) * 2
+                                        cost3 = round(cost3, 0)
+                                    else:        
+                                        coins = coins - cost3
+                                        cost3 = cost3 * 5
+                                        autog = autog * 2
+                                        cost3 = round(cost3, 0)
+                                # print(button)
+                            elif(button == 'golden_nugget' and is_showing_nugget == True):
+                                coins += 30000
+                                is_showing_nugget = False
+                                gameDisplay.fill(grey)
                             else:
-                                print("no button clicked")
+                                #For now
+                                pass
+                            
 
                 if coins == 2147483647:
                     print("You Beat the game!")
@@ -163,24 +158,27 @@ def main_loop():
         #Draw stuff
         for button in buttons:
             if(button == 'clicker_button'):
+                pygame.draw.rect(gameDisplay, moon_glow, buttons[button])
+
+            elif(button != 'golden_nugget'):
                 pygame.draw.rect(gameDisplay, black, buttons[button])
-            else:
-                pygame.draw.rect(gameDisplay, light_grey, buttons[button])
+
+            if (button == 'golden_nugget' and is_showing_nugget == True):
+                pygame.draw.rect(gameDisplay, yellow, buttons[button])
 
         DrawText("Clicker Scape", black, white, 400, 100, 50)
-        DrawText("You have " + str(f'{coins:.2f}') + " coins", black, white, 100, 50, 20)
-        DrawText("Upgrade clicker " + str(cost), black, white, 700, 300, 20)
-        DrawText("Buy auto clicker " + str(cost2), black, white, 150, 370, 20)
-        DrawText("Double Up! " + str(cost3), black, white, 350, 550, 20)
+        DrawText("You have " + str(f'{coins:.2f}') + " coins", black, white, 150, 50, 20)
+        DrawText("Upgrade clicker " + str(cost), black, white, 650, 270, 20)
+        DrawText("Buy auto clicker " + str(cost2), black, white, 150, 350, 20)
+        DrawText("Double Up Multiplier! " + str(cost3), black, white, 400, 550, 20)
         DrawText("Version: " + ver, black, white, 650, 50, 20)
-
-        # generate_nugget()
+        DrawText("Auto Click Multiplier: " + str(autog), black, white, 150, 370, 20)
+        DrawText("Clicker Multiplier: " + str(round(mong, 2)), black, white, 650, 290, 20)
 
         pygame.display.update()
         clock.tick(60)                        
 
 # Ending the program
-
 main_loop()
 pygame.quit()
 quit()
